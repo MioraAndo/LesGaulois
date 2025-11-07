7de8661c8925c529b8d326b7a20b477111fa9203
MioraAndo <andoniavorazanadrakoto@gmail.com>
2025-11-07 08:31:56 +0100

    test 1 2 1 2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -8,11 +8,11 @@ public class Gaulois {
 	}
 
 	private String nom;
-//	private int force;
 	private int effetPotion =1;
 	private Village village;
-	private int force, nb_trophees;
-	private Equipement trophees[] = new Equipement[100];
+	private int force;
+	private int nbTrophees;
+	private Equipement[] trophees = new Equipement[100];
 	
 
 	public String getNom() {
@@ -30,13 +30,9 @@ public class Gaulois {
 
 	}
 
-//	private String prendreParole() {
-//		return "Le Gaulois "+ nom + " : ";
-//	}
 	
 	private String prendreParole() { 
-		String texte = "Le gaulois " + nom + " : "; 
-		return texte; 
+		return "Le gaulois " + nom + " : "; 
 	} 
 	
 	public static void main(String[] args) {
@@ -49,34 +45,31 @@ public class Gaulois {
 		return nom;
 	}
 	
-//	public void frapper(Romain romain) {
-//		System.out.println(nom +  " envoie un grand coup dans la mâchoire de " + romain.getNom());
-//		romain.recevoirCoup(force /3);
-//		
-//	}
 	public void frapper(Romain romain) { 
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + 
 		romain.getNom()); 
-		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion); 
-		for (int i = 0; trophees != null && i < trophees.length; i++, 
+		Equipement[] coupe = romain.recevoirCoup((force / 3) * effetPotion); 
+		for (int i = 0; coupe != null && i < coupe.length; i++, 
 		nb_trophees++) { 
-		this.trophees[nb_trophees] = trophees[i]; 
+		this.trophees[nbTrophees] = trophees[i]; 
 		} 
-		return; 
+		 
 	}
 	public void boirePotion(int effet) {
         effetPotion = effet;
     }
 	
 	public void sePresenter() {
+		String texte1 = "Le Gaulois ";
+		String texte2 = " : \"Bonjour, je m'appelle ";
 	    if (this instanceof Chef) {
-	        System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+	        System.out.println(texte1+ nom + texte2 + nom +
 	                ". Je suis le chef du village " + village.getNom() + ".\"");
 	    } else if (village != null) {
-	        System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+	        System.out.println(texte1 + nom + texte2 + nom +
 	                ". J'habite le village " + village.getNom() + ".\"");
 	    } else {
-	        System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom +
+	        System.out.println(texte1 + nom + texte2 + nom +
 	                ". Je voyage de villages en villages.\"");
 	    }
 	}
